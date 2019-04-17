4dd609250faa5b7e1dbe052ff6fd920f7bc47a2c
David Pancic <pancicrdavid@gmail.com>
2019-04-17 14:36:29 +0100

    Add cool name element

--- js/navigation.js
+++ js/navigation.js
@@ -1,17 +1,29 @@
-// locate the navigation button element
-navButton = document.getElementById("nav-button");
+{ // Hide from global scope
+  // Remove showoff class from name-container
 
-// locate the navigation list
-navList = document.getElementById("nav-list");
+  // locate name-container
+  nameCont = document.getElementById("name-container");
 
-// add click event listener to navButton
-navButton.addEventListener('click', function(event) {
-  if (navList.classList.contains("hide")) {
-    navList.classList.remove("hide");
-    navButton.style["background-image"] = 'url("images/miscellaneous/close_icon.svg")';
-  }
-  else {
-    navList.classList.add("hide");
-    navButton.style["background-image"] = 'url("images/miscellaneous/navigation_icon.svg")';
-  };
-});
+  // remove showoff class
+  nameCont.classList.remove("showoff");
+
+  // Add navigation button functionality
+
+  // locate the navigation button element
+  navButton = document.getElementById("nav-button");
+
+  // locate the navigation list
+  navList = document.getElementById("nav-list");
+
+  // add click event listener to navButton
+  navButton.addEventListener('click', function(event) {
+    if (navList.classList.contains("hide")) {
+      navList.classList.remove("hide");
+      navButton.style["background-image"] = 'url("images/miscellaneous/close_icon.svg")';
+    }
+    else {
+      navList.classList.add("hide");
+      navButton.style["background-image"] = 'url("images/miscellaneous/navigation_icon.svg")';
+    };
+  });
+};
