90dce36748be4cc1ad2bc2448f24dc3e41a656c4
David Pancic <pancicrdavid@gmail.com>
2019-04-20 17:04:49 +0100

    Add technologies section and add extended background images

--- js/navigation.js
+++ js/navigation.js
@@ -2,7 +2,7 @@
   // Remove showoff class from name-container
 
   // locate name-container
-  nameCont = document.getElementById("name-container");
+  const nameCont = document.getElementById("name-container");
 
   // remove showoff class
   nameCont.classList.remove("showoff");
@@ -10,20 +10,30 @@
   // Add navigation button functionality
 
   // locate the navigation button element
-  navButton = document.getElementById("nav-button");
+  const navButton = document.getElementById("nav-button");
 
   // locate the navigation list
-  navList = document.getElementById("nav-list");
+  const navList = document.getElementById("nav-list");
 
   // add click event listener to navButton
   navButton.addEventListener('click', function(event) {
     if (navList.classList.contains("hide")) {
       navList.classList.remove("hide");
       navButton.style["background-image"] = 'url("images/miscellaneous/close_icon.svg")';
+
+      // remove transparent class from navList children
+      for (child of navList.children) {
+        child.classList.remove("transparent");
+      };
     }
     else {
       navList.classList.add("hide");
       navButton.style["background-image"] = 'url("images/miscellaneous/navigation_icon.svg")';
+
+      // add transparent class to navList children
+      for (child of navList.children) {
+        child.classList.add("transparent");
+      };
     };
   });
 };
---
+++ js/technologies.js
@@ -0,0 +1,89 @@
+{ // hide from global scope
+
+function logosEffectNarrow(vh, vw) {
+  // locate technology sections
+  const techSections = document.getElementsByClassName("technology");
+
+  for (section of [...techSections]) {
+
+    // find distance from top of viewport and top and bottom of sections
+    let rect = section.getBoundingClientRect();
+    let top = rect.top;
+    let bot = rect.bottom;
+
+    // add or remove class "emphasized" when section close to the top or
+    // bottom of the viewport
+    if (top < vh * 0.7 && bot > vh * 0.6) {
+      section.classList.add("emphasized");
+    }
+    else {
+      section.classList.remove("emphasized");
+    };
+  };
+};
+
+function logosEffectWide(vh, vw) {
+  // locate the logo images
+  const logos = document.getElementsByClassName("logo");
+
+  // locate the tech-intro
+  const intro = document.getElementById("tech-intro");
+
+  // locate one of the technology lists
+  const list = document
+    .getElementsByClassName("technology")[0]
+    .getElementsByTagName("ul")[0];
+
+  // find the distance from the bottom of the #tech-intro and top of viewport
+  let introBot = intro.getBoundingClientRect().bottom;
+
+  // find the distance from the top of one of the lists and top of viewport
+  let listTop = list.getBoundingClientRect().top;
+
+  // testing
+  console.log(`intro: ${Math.round(introBot)} < ${Math.round(vh)} ${introBot < vh} listTop: ${Math.round(listTop)} > ${Math.round(vh)} ${listTop > vh*0.9}`);
+  // console.log(`${Math.round((bot / vh) * 100) / 100}`);
+  // console.log(`${Math.round((listTop / vh) * 100) / 100}`);
+
+  // add or remove class "together" when appropriate
+  if (introBot < vh * 1 && listTop > vh * 1) {
+    for (logo of logos) {
+      logo.classList.add("together");
+    };
+  }
+  else {
+    for (logo of logos) {
+      logo.classList.remove("together");
+    };
+  };
+};
+
+function logosEffect() {
+  // get vieport height and width
+  // source: https://stackoverflow.com/a/8876069
+  let vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  let vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+  // effects for a screen too narrow for 3 columns
+  if (vw < 950) {
+    logosEffectNarrow(vh, vw);
+  }
+  // effects for a screen wide enough for 3 columns
+  else {
+    logosEffectWide(vh, vw);
+  };
+}
+
+// add scroll event
+let scrollTriggered = false;
+window.addEventListener('scroll', function(e) {
+  if (!scrollTriggered) {
+    scrollTriggered = true;
+    logosEffect();
+    window.setTimeout(function () {
+      scrollTriggered = false;
+    }, 100);
+  };
+});
+
+};
